7b2256bca43bb89fa2fbeb8906d7eef77244b844
JunrenHuang <hjr@JunrenHuangs-MacBook-Pro.local>
2017-05-15 17:05:02 +0800

    每行最少有三个item

--- CollectionViewElCapitan/TMCollectionLayout.swift
+++ CollectionViewElCapitan/TMCollectionLayout.swift
@@ -4,10 +4,12 @@ class TMCollectionLayout: NSCollectionViewFlowLayout {
 
     // 缓存单元宽度和每行的单元个数
     var currentWidth: CGFloat = 0
-    var currentCount: CGFloat = 0
+    var currentCount = 0
 
     // 最小单元宽度
     let minWidth: CGFloat = 140
+    // 每行最小单元个数
+    let minRowCount = 3
 
     // 单元之间的间隔
     let X_PADDING: CGFloat = 10.0
@@ -17,29 +19,30 @@ class TMCollectionLayout: NSCollectionViewFlowLayout {
         for attributes in layoutAttributesArray {
             let width = rect.width
 
-            var newCount: CGFloat
+            var newCount: Int
             var newWidth: CGFloat
             if width == currentWidth {
                 newCount = currentCount
                 newWidth = currentWidth
             } else {
-                newCount = CGFloat(Int(width / (minWidth + X_PADDING)))
-                newCount += 1
-                while width > (newCount * minWidth + X_PADDING * (newCount - 1)) {
+                newCount = Int(width / (minWidth + X_PADDING)) + 1
+                while width > (CGFloat(newCount) * minWidth + X_PADDING * CGFloat(newCount - 1)) {
                     newCount += 1
                 }
                 newCount -= 1
+                newCount = max(newCount, minRowCount)
 
-                newWidth = (width - X_PADDING * (newCount - 1)) / newCount
+                newWidth = (width - X_PADDING * CGFloat(newCount - 1)) / CGFloat(newCount)
+                newWidth = max(minWidth, newWidth)
 
                 currentWidth = newWidth
                 currentCount = newCount
             }
 
-            if let index = (attributes.indexPath as NSIndexPath?)?.item {
+            if let itemIndex = (attributes.indexPath as NSIndexPath?)?.item {
 
-                let col = index % Int(newCount)
-                let row = index / Int(newCount)
+                let col = itemIndex % Int(newCount)
+                let row = itemIndex / Int(newCount)
                 let originX = CGFloat(col) * (newWidth + X_PADDING)
                 let originY = CGFloat(row) * (newWidth + X_PADDING)
 
